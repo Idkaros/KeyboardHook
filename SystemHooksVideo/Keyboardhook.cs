e759c6dd4c966e9b8486c6ca65bae51eb03002b5
virgiliomca <virgiliomca@gmail.com>
2022-10-24 08:28:16 -0300

    Primera protección

---
+++ SystemHooksVideo/Keyboardhook.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SystemHooksMicrosoft
+{
+    public class Keyboardhook
+    {
+        const int WM_KEYDOWN = 0x100;
+        const int WM_KEYUP = 0x101;
+        const int WM_SYSKEYDOWN = 0x105;
+        [StructLayout(LayoutKind.Sequential)]
+        private struct KBDLLHOOKSTRUCT
+        {
+            public Keys key;
+            public int vkCode;
+            public int scanCode;
+            public int flags;
+            public IntPtr extra;
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, int wParam, int lParam);
+        private LowLevelKeyboardProc keyboardProcess;
+
+        public static IntPtr ptrHook;
+
+        public event KeyEventHandler KeyUp;
+        public event KeyEventHandler KeyDown;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int id, LowLevelKeyboardProc callback, IntPtr hMod, uint dwThreadId);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hook, int nCode, int wp, IntPtr lp);
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string name);
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool UnhookWindowsHookEx(IntPtr hook);
+
+        public void Hook()
+        {
+            ProcessModule processModule = Process.GetCurrentProcess().MainModule();
+        }
+    }
+}
