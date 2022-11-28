0148379f2a7060a0ab99b87cdbdf12d5661609f6
virgiliomca <virgiliomca@gmail.com>
2022-11-28 20:47:05 -0300

    El proyecto "google upload library"

---
+++ gul/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace gul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        private static Google.Apis.Drive.v3.DriveService GetService()
+        {
+            //Google.Apis.Json
+            //Google.Apis.Drive.v3.Data
+            
+            var tokenResponse = new Google.Apis.Auth.OAuth2.Responses.TokenResponse
+            {
+                AccessToken = "...",
+                RefreshToken = "...",
+            }
+
+
+            var applicationName = "..."; // Use the name of the project in Google Cloud
+            var username = "..."; // Use your email
+
+
+            var apiCodeFlow = new Google.Apis.Auth.OAuth2.Flows.GoogleAuthorizationCodeFlow(new Google.Apis.Auth.OAuth2.Flows.GoogleAuthorizationCodeFlow.Initializer
+            {
+                ClientSecrets = new ClientSecrets
+                {
+                    ClientId = "...",
+                    ClientSecret = "..."
+                },
+                Scopes = new[] { Scope.Drive },
+                DataStore = new FileDataStore(applicationName)
+            })
+
+
+            var credential = new UserCredential(apiCodeFlow, username, tokenResponse)
+
+
+            var service = new DriveService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = applicationName
+            })
+            return service;
+        }
+    }
+}
---
+++ gul/Program2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace gul
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+
+            // Load the Service account credentials and define the scope of its access.
+            
+            var credential = Google.Apis.Auth.OAuth2.GoogleCredential.FromFile(PathToServiceAccountKeyFile)
+                            .CreateScoped(Google.Apis.Drive.v3.DriveService.ScopeConstants.Drive);
+
+            var service = new Google.Apis.Drive.v3.DriveService(new Google.Apis.Services.BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential
+            });
+        }
+
+        private const string PathToServiceAccountKeyFile = @"C:\Youtube\dev\ServiceAccountCred.json";
+        private const string ServiceAccountEmail = "driveuploadtest@testapikey-305109.iam.gserviceaccount.com";
+        private const string UploadFileName = "Test hello.txt";
+        private const string DirectoryId = "10krlloIS2i_2u_ewkdv3_1NqcpmWSL1w";
+    }
+}
